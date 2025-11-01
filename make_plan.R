1b4fcc3f7111860afa1687c628707c2542ee2448
rvcrawford <rvcrawford@gmail.com>
2025-11-01 14:33:42 -0400

    make plan

---
+++ make_plan.R
@@ -0,0 +1,29 @@
+# experimental design
+library(tidyverse)
+set.seed(123898)
+trts <- tibble(n_rye = c(0,2,4)) |> 
+  mutate(n_pea = 4-n_rye, trt_name = c("all_pea", "half_n_half", "all_rye")) |> 
+  # set n reps to 5
+  uncount(5) |> 
+  mutate(box = c(1,
+                 11,
+                 34,
+                 6,
+                 25,
+                 43,
+                 38,
+                 21,
+                 17,
+                 39,
+                 23,
+                 18,
+                 42,
+                 19,
+                 3
+                 )) |> 
+  mutate(rep = 1:5, .by = trt_name) |> 
+  arrange(rep) |> 
+  slice_sample(prop = 1,by = rep) |> 
+  mutate(box_order = 1:n())
+
+
